8729c743884879f8ce0ef9b00f27dce8fa46331d
Rahul_Yadav <68394509+rahulyadav232@users.noreply.github.com>
2025-02-24 12:41:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const app = express();
 // app.use(cors());
 // app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cors({
-  origin: ["https://your-frontend.vercel.app"], // Allow requests from your frontend
+  origin: ["hrms-psquare-11p8.vercel.app"], // Allow requests from your frontend
   methods: ["GET", "POST", "PUT", "DELETE"], // Allowed methods
   credentials: true // Allow cookies (if needed)
 }));
